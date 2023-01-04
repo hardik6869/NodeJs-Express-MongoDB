9c44dc93bcf8204aaab65b50e01fef338b7a6194
hardik6869 <sorthiyahardik6869@gmail.com>
2023-01-04 12:34:55 +0530

    Respond with Data

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const http = require("http");
+
+const todos = [
+  { id: 1, text: "Todo One" },
+  { id: 2, text: "Todo Two" },
+  { id: 3, text: "Todo Three" },
+];
+
 const server = http.createServer((req, res) => {
-  const { headers, url, method } = req;
-  console.log(headers, url, method);
-  res.end();
+  // res.setHeader("Content-Type", "text/html");
+  // res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("X-Powered-By", "Node.js");
+  res.write("<h1> Hello </h1>");
+  res.write("<h2> How Are You? </h2>");
+  res.end(
+    JSON.stringify({
+      success: true,
+      data: todos,
+    })
+  );
 });
 
 const PORT = 5000;
