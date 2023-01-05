5deb5f7dbd52452371b55fdde2ab196f320eed65
hardik6869 <sorthiyahardik6869@gmail.com>
2023-01-05 11:04:18 +0530

    Create Routes using the express router

--- server.js
+++ server.js
@@ -1,33 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Route Files
+const bootcamps = require("./routes/bootcamps");
+
+// Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.get("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show All Bootcamp" });
-});
-
-app.get("/api/v1/bootcamps/:id", (req, res) => {
-  res.status(200).json({ success: true, msg: `Get Bootcamp ${req.params.id}` });
-});
-
-app.post("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, msg: "Create New Bootcamp" });
-});
-
-app.put("/api/v1/bootcamps/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update Bootcamp ${req.params.id}` });
-});
-
-app.delete("/api/v1/bootcamps/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
-});
+// Mount Routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5200;
 app.listen(
