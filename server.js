06d884349e32da382903f9bbb263b1a5e60e8a06
hardik6869 <sorthiyahardik6869@gmail.com>
2023-01-05 12:06:39 +0530

    Create Custom Middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const logger = require("./middleware/logger");
 
 // Route Files
 const bootcamps = require("./routes/bootcamps");
@@ -8,6 +9,9 @@ const bootcamps = require("./routes/bootcamps");
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Custom Middleware
+app.use(logger);
 
 // Mount Routers
 app.use("/api/v1/bootcamps", bootcamps);
