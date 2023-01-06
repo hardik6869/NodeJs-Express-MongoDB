f8d9fb2bf19443c707c12a0b1bc3f1fad33d4a41
hardik6869 <sorthiyahardik6869@gmail.com>
2023-01-06 12:32:28 +0530

    Use colors package in the console

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const colors = require("colors");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -25,12 +26,14 @@ app.use("/api/v1/bootcamps", bootcamps);
 const PORT = process.env.PORT || 5200;
 const server = app.listen(
   PORT,
-  console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle Unhandle promise rejections
 process.on("unhandleRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red.bold);
   // Close server & exit process
   server.close(() => process.exit(1));
 });
