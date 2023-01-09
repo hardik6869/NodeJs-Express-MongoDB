8ab87e13cd1287e4701b65f5088913ca4fdc01db
hardik6869 <sorthiyahardik6869@gmail.com>
2023-01-09 14:21:32 +0530

    Fetch Bootcamps data

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,31 @@ const Bootcamp = require("../models/Bootcamp");
 // @desc    Get All Bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show All Bootcamp" });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.find();
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Get All Bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Get Bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ seccess: false });
+  }
 };
 
 // @desc    Create New Bootcamp
