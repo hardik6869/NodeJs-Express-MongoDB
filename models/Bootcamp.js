f87d94cf4581fcdfdb3edcdfe81876bccdac238b
hardik6869 <sorthiyahardik6869@gmail.com>
2023-01-09 12:07:43 +0530

    Create Bootcamp

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -41,11 +41,10 @@ const BootcampSchema = new mongoose.Schema({
     type: {
       type: String, // Don't do `{ location: { type: String } }`
       enum: ["Point"], // 'location.type' must be 'Point'
-      required: true,
     },
     coordinates: {
       type: [Number],
-      required: true,
+      // required: true,
       index: "2dsphere",
     },
     formattedAddress: String,
