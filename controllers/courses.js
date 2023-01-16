3f43d4655fb67783344766a9fb0370e5e187cc53
hardik6869 <sorthiyahardik6869@gmail.com>
2023-01-16 11:19:51 +0530

    Get Single Course & Add Spacific Bootcamp Course

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const Course = require("../models/Course");
+const Bootcamp = require("../models/Bootcamp");
 
 // @desc    GET Courses
 // @route   GEt /api/v1/courses
@@ -20,4 +21,41 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   }
   const courses = await query;
   res.status(200).json({ success: true, count: courses.length, data: courses });
+});
+
+// @desc    GET Single Courses
+// @route   GET /api/v1/courses/:id
+// @access  Public
+
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: "bootcamp",
+    select: "name description",
+  });
+  if (!course) {
+    return next(
+      new ErrorResponse(`No Course with the id of ${req.params.id}`),
+      404
+    );
+  }
+  res.status(200).json({ success: true, count: course.length, data: course });
+});
+
+// @desc    Add Courses
+// @route   POST /api/v1/bootcamps/:bootcampId/courses/:id
+// @access  Private
+
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`),
+      404
+    );
+  }
+
+  const course = await Course.create(req.body);
+
+  res.status(200).json({ success: true, count: course.length, data: course });
 });
