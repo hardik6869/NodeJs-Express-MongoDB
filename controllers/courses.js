052ef1a3ad085c39685dfe2bd5a83f21f894fee9
hardik6869 <sorthiyahardik6869@gmail.com>
2023-01-16 11:42:30 +0530

    Update & Delete Course

--- controllers/courses.js
+++ controllers/courses.js
@@ -7,7 +7,6 @@ const Bootcamp = require("../models/Bootcamp");
 // @route   GEt /api/v1/courses
 // @route   GET /api/v1/bootcamps/:bootcampId/courses
 // @access  Public
-
 exports.getCourses = asyncHandler(async (req, res, next) => {
   let query;
 
@@ -26,7 +25,6 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
 // @desc    GET Single Courses
 // @route   GET /api/v1/courses/:id
 // @access  Public
-
 exports.getCourse = asyncHandler(async (req, res, next) => {
   const course = await Course.findById(req.params.id).populate({
     path: "bootcamp",
@@ -44,7 +42,6 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @desc    Add Courses
 // @route   POST /api/v1/bootcamps/:bootcampId/courses/:id
 // @access  Private
-
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
@@ -57,5 +54,42 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
 
   const course = await Course.create(req.body);
 
-  res.status(200).json({ success: true, count: course.length, data: course });
+  res.status(200).json({ success: true, data: course });
+});
+
+// @desc    Update Courses
+// @route   PUT /api/v1/courses/:id
+// @access  Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course   with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ success: true, data: course });
+});
+
+// @desc    delete Courses
+// @route   DELETE /api/v1/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course   with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  await course.remove();
+
+  res.status(200).json({ success: true, data: {} });
 });
