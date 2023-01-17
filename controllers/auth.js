b77ff45c7b3a302d90250cfd0051eae47a915a7f
hardik6869 <sorthiyahardik6869@gmail.com>
2023-01-17 14:38:30 +0530

    Get JSON Web Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,5 +15,9 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role,
   });
-  res.status(200).json({ success: true });
+
+  //Create Token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
