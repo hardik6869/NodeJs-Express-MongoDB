1859c95e7956886e965c0818f9293fa24663a0a7
hardik6869 <sorthiyahardik6869@gmail.com>
2023-01-23 12:15:47 +0530

    Added: XSS Protection & Security Headers

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const colors = require("colors");
 const errorHandler = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -40,6 +42,12 @@ app.use(fileUpload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set Security Headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
